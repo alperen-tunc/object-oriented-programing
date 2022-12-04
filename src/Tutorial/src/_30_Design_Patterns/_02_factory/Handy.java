4d75510a8b5a4223da113fd290b7ece7c0bc4a19
alperentuncc <115324004+alperen-tunc@users.noreply.github.com>
2022-12-04 16:04:42 +0100

    update_fromHome_04.12

---
+++ src/Tutorial/src/_30_Design_Patterns/_02_factory/Handy.java
@@ -0,0 +1,9 @@
+package Tutorial.src._30_Design_Patterns._02_factory;
+
+public interface Handy
+{
+    String getModel();
+    String getBattery();
+    String getWidth();
+    String getHeight();
+}
