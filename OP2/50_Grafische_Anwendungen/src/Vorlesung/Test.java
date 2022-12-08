8563d2acc6b010874237216086639570c45a2c0b
Alperen Tunc <tuncalperen@mail.de>
2022-12-08 10:36:17 +0100

    update_08.12

---
+++ OP2/50_Grafische_Anwendungen/src/Vorlesung/Test.java
@@ -0,0 +1,17 @@
+package Vorlesung;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Test
+{
+    public static void main(String[] args)
+    {
+        Button button = new Button("OK"); // Schaltfläche in AWT
+        JButton button1 = new JButton("OK"); // Schaltfläche in Swing
+
+
+
+        System.out.println("\n\nEnd of Main..");
+    }
+}
