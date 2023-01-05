e00a73747ebe29400ceb7468d00eb30e2a74276a
Alperen Tunc <tuncalperen@mail.de>
2023-01-05 08:56:37 +0100

    update_05.01

--- OP3/30_crud_operation/src/Main.java
+++ OP3/30_crud_operation/src/Main.java
@@ -1,11 +1,12 @@
 import java.math.BigDecimal;
+import java.math.MathContext;
 
 public class Main
 {
 
     public static void main(String[] args)
     {
-
+        BigDecimal bigDecimal;
         System.out.println("Hello world!");
     }
 }
